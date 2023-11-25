17e98a5060c92210a246f1cb5811ed3bf4e462e4
ronalsteven <149214397+ronalsteven@users.noreply.github.com>
2023-11-24 20:33:27 -0500

    detalles

--- pagina/src/App.js
+++ pagina/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Logo from './componentes/Logo/Logo';
 import Navbar from './componentes/Navbar/Navbar';
 import Carousel from './componentes/Carousel/Carousel';
-import ShoppingCartIcon from './componentes/ShoppingCartIcon/ShoppingCartIcon';
 import FeaturedItems from './componentes/FeaturedItems/FeaturedItems';
 import StylishDogs from './componentes/StylishDogs/StylishDogs'; // Importa el componente StylishDogs
 import StylishCats from './componentes/StylishCats/StylishCats'; // Importa el componente StylishCats
@@ -14,7 +13,6 @@ function App() {
       <header className="App-header"> 
         <Logo />
         <Navbar />
-        <ShoppingCartIcon />
       </header>
       <Carousel />
       <FeaturedItems />
--- pagina/src/componentes/Logo/Logo.js
+++ pagina/src/componentes/Logo/Logo.js
@@ -5,7 +5,7 @@ import logo from './logo.png'; // Importa tu logo
 
 const Logo = () => {
     return (
-        <img src={logo} alt="Logo" className="logo" /> // Aplica la clase "logo"
+        <img src={logo} alt="Logo" className="logo" /> // Aplica la clase "logo"//
     );
 };
 
